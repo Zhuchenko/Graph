452ea89c6ae9ac11bfafe2c6973a87db51994631
FIRM\KristinaZ <kristinafrench7@gmail.com>
2017-07-03 17:31:25 +0300

    Added some checks

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -17,12 +17,25 @@ namespace Graph
             Console.Write("F = ");
             string F = Console.ReadLine();
 
+            if (graph == null)
+            {
+                Console.WriteLine("Every name of edge must be one and only!");
+                Console.ReadKey();
+                return;
+            }
+            if (!Contains(graph, S) || !Contains(graph, F))
+            {
+                Console.WriteLine("Graph must contain S and F");
+                Console.ReadKey();
+                return;
+            }
+
             var result = new List<List<Edge>>();
 
             var firstEdges = from item in graph where item.Starting == S select item;
             foreach (Edge e in firstEdges)
             {
-                var edges = new List<Pair<Edge,List<Edge>>> { new Pair<Edge, List<Edge>>(e, new List<Edge>()) };
+                var edges = new List<Pair<Edge, List<Edge>>> { new Pair<Edge, List<Edge>>(e, new List<Edge>()) };
                 while (edges.Count > 0)
                 {
                     if (edges.Last().Key.Final == F)
@@ -33,9 +46,8 @@ namespace Graph
                     }
                     Edge nextEdge = null;
                     foreach (Edge item in graph)
-                        if (edges.Count > 0
-                            && item.Starting == edges.Last().Key.Final
-                            && !edges.Last().Value.Contains(item) 
+                        if (item.Starting == edges.Last().Key.Final
+                            && !edges.Last().Value.Contains(item)
                             && edges.FindAll(x => x.Key == item).Count == 0)
                             nextEdge = item;
                     if (nextEdge != null)
@@ -45,12 +57,24 @@ namespace Graph
                     }
                     else
                     {
-                        if (edges.Count > 1)
+                        //if (edges.Count > 1)
                             edges.Remove(edges.Last());
-                        else break;
+                        //else break;
                     }
                 }
             }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine("Path #" + (i + 1));
+                foreach (var item in result[i])
+                    Console.Write("  " + item);
+                Console.WriteLine();
+            }
+
+            if (result.Count == 0)
+                Console.WriteLine("No path");
+
             Console.ReadKey();
         }
 
@@ -66,12 +90,25 @@ namespace Graph
                     string[] param = sr.ReadLine().Split(del, StringSplitOptions.RemoveEmptyEntries);
                     int index = 0;
                     foreach (Edge item in returnValue)
+                    {
+                        if (item.Name == param[2])
+                            return null;
                         if (item.Starting == param[0] && item.Final == param[1])
                             index++;
+                    }
                     returnValue.Add(new Edge(param[0], param[1], index, param[2]));
                 }
             }
             return returnValue;
+        }
+
+        public static bool Contains(IEnumerable<Edge> graph, string vertex)
+        {
+            foreach(var item in graph)
+                if (item.Starting == vertex || item.Final == vertex)
+                    return true;
+            
+            return false;
         }
     }
 }
